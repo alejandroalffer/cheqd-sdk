499a04fd3a380ec578a8af48f89a81a495f63d4d
Vladan Divac <vladan.divac@evernym.com>
2019-11-25 22:52:20 +0100

    Remove unwanted print out to console
    Signed-off-by: Vladan Divac <vladan.divac@evernym.com>

--- vcx/libvcx/src/issuer_credential.rs
+++ vcx/libvcx/src/issuer_credential.rs
@@ -370,7 +370,6 @@ impl IssuerCredential {
         //Todo: make a cred_def_offer error
         let libindy_offer = anoncreds::libindy_issuer_create_credential_offer(&self.cred_def_id)?;
 
-        println!("remote_did {:?}", self.remote_did);
         let (libindy_offer, cred_def_id) =
             if !Qualifier::is_fully_qualified(&self.remote_did) {
                 (anoncreds::libindy_to_unqualified(&libindy_offer)?,
