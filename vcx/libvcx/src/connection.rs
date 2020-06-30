f83af91e5e20e98aa9533fab04812446c74221df
artem.ivanov <artem.ivanov@dsr-company.com>
2020-06-30 11:52:27 +0300

    Ignored `conection_redirect` test
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- vcx/libvcx/src/connection.rs
+++ vcx/libvcx/src/connection.rs
@@ -1853,6 +1853,7 @@ pub mod tests {
 
     #[cfg(feature = "agency")]
     #[cfg(feature = "pool_tests")]
+    #[ignore]
     #[test]
     fn test_connection_redirection_real() {
         let _setup = SetupLibraryAgencyV1::init();
