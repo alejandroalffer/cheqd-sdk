48fe4987afdbdb8888ad899f241627ea6ddab8fa
artem.ivanov <artem.ivanov@dsr-company.com>
2020-07-08 17:13:55 +0300

    MSDK-196: VCX:  fixed review comment
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- vcx/libvcx/src/v3/handlers/connection/agent.rs
+++ vcx/libvcx/src/v3/handlers/connection/agent.rs
@@ -150,11 +150,11 @@ impl AgentInfo {
         Ok(message)
     }
 
-    pub fn send_message(&self, message: &A2AMessage, did_dod: &DidDoc) -> VcxResult<()> {
-        trace!("Agent::send_message >>> message: {:?}, did_doc: {:?}", message, did_dod);
+    pub fn send_message(&self, message: &A2AMessage, did_doc: &DidDoc) -> VcxResult<()> {
+        trace!("Agent::send_message >>> message: {:?}, did_doc: {:?}", message, did_doc);
         let pw_key = if self.pw_vk.is_empty() { None} else {Some(self.pw_vk.clone())};
-        let envelope = EncryptionEnvelope::create(&message, pw_key.as_ref().map(String::as_str), &did_dod)?;
-        httpclient::post_message(&envelope.0, &did_dod.get_endpoint())?;
+        let envelope = EncryptionEnvelope::create(&message, pw_key.as_ref().map(String::as_str), &did_doc)?;
+        httpclient::post_message(&envelope.0, &did_doc.get_endpoint())?;
         trace!("Agent::send_message <<<");
         Ok(())
     }
