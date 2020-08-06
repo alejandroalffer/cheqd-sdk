f438bc443f8954dff59d9e671bcbfee3cf309ec1
artem.ivanov <artem.ivanov@dsr-company.com>
2020-08-06 16:50:33 +0300

    Added parsing of Agency response for some codes
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- vcx/wrappers/java/src/main/java/com/evernym/sdk/vcx/vcx/InvalidAgencyResponseException.java
+++ vcx/wrappers/java/src/main/java/com/evernym/sdk/vcx/vcx/InvalidAgencyResponseException.java
@@ -7,13 +7,13 @@ import com.evernym.sdk.vcx.VcxException;
  * Created by abdussami on 05/06/18.
  */
 
-public class InvalidMessagesException extends VcxException
+public class InvalidAgencyResponseException extends VcxException
 {
     private static final long serialVersionUID = 3294831240096535507L;
     private final static String message = "VCX Exception";
 
 
-    public InvalidMessagesException()
+    public InvalidAgencyResponseException()
     {
         super(message, ErrorCode.INVALID_AGENCY_RESPONSE.value());
     }
