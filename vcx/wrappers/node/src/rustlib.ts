Artemkaaas/indy-sdk

MP-73: Implement inviteAction protocol in Libvcx
Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

vcx/wrappers/node/src/rustlib.ts
@@ -139,6 +139,7 @@ export interface IFFIEntryPoint {
                                            comment: string | null | undefined, cb: any) => number,
   vcx_connection_send_reuse: (commandId: number, handle: number, invite: string, cb: any) => number,
   vcx_connection_send_answer: (commandId: number, handle: number, question: string, answer: string, cb: any) => number,
+  vcx_connection_send_invite_action: (commandId: number, handle: number, goalCode: string, cb: any) => number,
   vcx_connection_get_pw_did: (commandId: number, handle: number, cb: any) => number,
   vcx_connection_get_their_pw_did: (commandId: number, handle: number, cb: any) => number,
   vcx_connection_info: (commandId: number, handle: number, cb: any) => number,
@@ -370,6 +371,8 @@ export const FFIConfiguration: { [ Key in keyof IFFIEntryPoint ]: any } = {
     FFI_CALLBACK_PTR]],
   vcx_connection_send_answer: [FFI_ERROR_CODE, [FFI_COMMAND_HANDLE, FFI_CONNECTION_HANDLE, FFI_STRING_DATA,
     FFI_STRING_DATA, FFI_CALLBACK_PTR]],
+  vcx_connection_send_invite_action: [FFI_ERROR_CODE, [FFI_COMMAND_HANDLE, FFI_CONNECTION_HANDLE, FFI_STRING_DATA,
+    FFI_CALLBACK_PTR]],
   vcx_connection_get_pw_did: [FFI_ERROR_CODE, [FFI_COMMAND_HANDLE, FFI_CONNECTION_HANDLE, FFI_CALLBACK_PTR]],
   vcx_connection_get_their_pw_did: [FFI_ERROR_CODE, [FFI_COMMAND_HANDLE, FFI_CONNECTION_HANDLE, FFI_CALLBACK_PTR]],
   vcx_connection_redirect: [FFI_ERROR_CODE, [FFI_COMMAND_HANDLE, FFI_CONNECTION_HANDLE, FFI_CONNECTION_HANDLE,

alejandroalffer/cheqd-sdk

vcx/wrappers/node/src/rustlib.ts
@@ -139,6 +139,7 @@ export interface IFFIEntryPoint {
                                            comment: string | null | undefined, cb: any) => number,
   vcx_connection_send_reuse: (commandId: number, handle: number, invite: string, cb: any) => number,
   vcx_connection_send_answer: (commandId: number, handle: number, question: string, answer: string, cb: any) => number,
+  vcx_connection_send_invite_action: (commandId: number, handle: number, data: string, cb: any) => number,
   vcx_connection_get_pw_did: (commandId: number, handle: number, cb: any) => number,
   vcx_connection_get_their_pw_did: (commandId: number, handle: number, cb: any) => number,
   vcx_connection_info: (commandId: number, handle: number, cb: any) => number,
@@ -370,6 +371,8 @@ export const FFIConfiguration: { [ Key in keyof IFFIEntryPoint ]: any } = {
     FFI_CALLBACK_PTR]],
   vcx_connection_send_answer: [FFI_ERROR_CODE, [FFI_COMMAND_HANDLE, FFI_CONNECTION_HANDLE, FFI_STRING_DATA,
     FFI_STRING_DATA, FFI_CALLBACK_PTR]],
+  vcx_connection_send_invite_action: [FFI_ERROR_CODE, [FFI_COMMAND_HANDLE, FFI_CONNECTION_HANDLE, FFI_STRING_DATA,
+    FFI_CALLBACK_PTR]],
   vcx_connection_get_pw_did: [FFI_ERROR_CODE, [FFI_COMMAND_HANDLE, FFI_CONNECTION_HANDLE, FFI_CALLBACK_PTR]],
   vcx_connection_get_their_pw_did: [FFI_ERROR_CODE, [FFI_COMMAND_HANDLE, FFI_CONNECTION_HANDLE, FFI_CALLBACK_PTR]],
   vcx_connection_redirect: [FFI_ERROR_CODE, [FFI_COMMAND_HANDLE, FFI_CONNECTION_HANDLE, FFI_CONNECTION_HANDLE,