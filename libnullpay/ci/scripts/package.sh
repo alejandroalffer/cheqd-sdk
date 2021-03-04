edb2ad2e2952f46bf558018f176d4061a92a57d6
Sergey Khoroshavin <sergey.khoroshavin@evernym.com>
2021-03-05 00:24:45 +0300

    VE-2400: Publish libnullpay
    Signed-off-by: Sergey Khoroshavin <sergey.khoroshavin@evernym.com>

--- libnullpay/ci/scripts/package.sh
+++ libnullpay/ci/scripts/package.sh
@@ -16,12 +16,13 @@ PACKAGE_TYPE=$(lsb_release -cs)
 # REVISION=$(git rev-parse HEAD | cut -c 1-7)
 VERSION=1.95.0~${BUILD_NUM}-${PACKAGE_TYPE}  # TODO: Autodetect main part
 
-pushd libindy
+pushd libnullpay
 cp Cargo.toml Cargo.toml.backup
-sed -i -E -e "/provides = \"libindy \(= [(,),0-9,.]+\)\"/a conflicts = \"libindy\"" Cargo.toml
-sed -i -E -e "s/provides = \"libindy \(= [(,),0-9,.]+\)\"/provides = \"libindy \(= ${VERSION}\)\"/g" Cargo.toml
-sed -i -E -e "s/name = \"libindy\"/name = \"libindy-async\"/g" Cargo.toml
+sed -i -E -e "s/depends = \"libindy \(= [(,),0-9,.]+\)\"/depends = \"libindy-async \(= ${VERSION}\)\"/g" Cargo.toml
+sed -i -E -e "/provides = \"libnullpay \(= [(,),0-9,.]+\)\"/a conflicts = \"libnullpay\"" Cargo.toml
+sed -i -E -e "s/provides = \"libnullpay \(= [(,),0-9,.]+\)\"/provides = \"libnullpay \(= ${VERSION}\)\"/g" Cargo.toml
+sed -i -E -e "s/name = \"libnullpay\"/name = \"libnullpay-async\"/g" Cargo.toml
 sed -i -E -e "/maintainer-scripts = \".\/debian\"/d" Cargo.toml
-cargo deb --no-build --deb-version ${VERSION} --variant libindy-${PACKAGE_TYPE}
+cargo deb --no-build --deb-version ${VERSION} --variant libnullpay-${PACKAGE_TYPE}
 mv -f Cargo.toml.backup Cargo.toml
 popd
