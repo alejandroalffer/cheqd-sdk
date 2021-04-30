a9740eb845768eda521f8db09e13c2d87eac80a8
toktar <renata.toktar@evernym.com>
2021-04-30 17:01:42 +0300

    Add `build_msg_create_nym`

--- libindy/src/services/ledger2.rs
+++ libindy/src/services/ledger2.rs
@@ -3,7 +3,8 @@
 use cosmos_sdk::bank::MsgSend;
 use cosmos_sdk::tx::{Msg, MsgType};
 use cosmos_sdk::Coin;
-use indy_api_types::errors::IndyResult;
+use indy_api_types::errors::{IndyResult, IndyErrorKind};
+use indy_api_types::IndyError;
 
 pub struct Ledger2Service {}
 
@@ -32,11 +33,35 @@ impl Ledger2Service {
 
         Ok(msg_send.to_msg()?)
     }
+
+    pub fn build_msg_create_nym(
+        &self,
+        alias: &str,
+        verkey: &str,
+        did: &str,
+        role: &str,
+        from: &str,
+    ) -> IndyResult<Msg> {
+
+        let msg_send = Nym {
+            creator: from.to_string(),
+            id: 0,
+            alias: alias.to_string(),
+            verkey: verkey.to_string(),
+            did: did.to_string(),
+            role: role.to_string(),
+        };
+
+        Ok(msg_send.to_msg()?)
+    }
 }
+
+
 
 #[cfg(test)]
 mod test {
     use crate::services::{KeysService, Ledger2Service, Pool2Service};
+    use cosmos_sdk::crypto::secp256k1::SigningKey;
 
     #[async_std::test]
     async fn test_tx_commit_flow() {
@@ -83,4 +108,50 @@ mod test {
 
         assert!(true)
     }
+    //
+    // #[async_std::test]
+    // async fn test_create_nym_flow() {
+    //     let ledger2_service = Ledger2Service::new();
+    //     let pool2_service = Pool2Service::new();
+    //     let keys_service = KeysService::new();
+    //
+    //     let alice = keys_service
+    //         .add_from_mnemonic("alice", "alice")
+    //         .await
+    //         .unwrap();
+    //     let bob = keys_service.add_from_mnemonic("bob", "bob").await.unwrap();
+    //
+    //     println!("Alice's account id: {}", alice.account_id);
+    //     println!("Bob's account id: {}", bob.account_id);
+    //
+    //     let msg = ledger2_service
+    //         .build_msg_create_nym("alias", "verkey", "did", "role", alice.alias.as_str())
+    //         .unwrap();
+    //
+    //     let tx = pool2_service
+    //         .build_tx(
+    //             &alice.pub_key,
+    //             vec![msg],
+    //             "verimcosmos",
+    //             9, // What is it?
+    //             0,
+    //             300000,
+    //             0u64,
+    //             "stake",
+    //             3909860,
+    //             "memo",
+    //         )
+    //         .unwrap();
+    //
+    //     let signed = keys_service.sign("alice", tx).await.unwrap();
+    //
+    //     // Broadcast
+    //
+    //     pool2_service
+    //         .send_tx_commit(signed, "http://localhost:26657")
+    //         .await
+    //         .unwrap();
+    //
+    //     assert!(true)
+    // }
 }
