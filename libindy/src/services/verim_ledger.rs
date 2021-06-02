c3c33bddcb4e87f94943607581da8acad250f04c
toktar <renata.toktar@evernym.com>
2021-06-02 11:15:17 +0300

    add get_proof_spec()

--- libindy/src/services/verim_ledger.rs
+++ libindy/src/services/verim_ledger.rs
@@ -327,4 +327,26 @@ mod test {
 
         println!("{:?}", decoded);
     }
+
+    pub fn get_proof_spec() -> ProofSpec {
+        ProofSpec {
+            leaf_spec: Some(LeafOp {
+                hash: HashOp::Sha256.into(),
+                prehash_key: HashOp::NoHash.into(),
+                prehash_value: HashOp::NoHash.into(),
+                length: LengthOp::NoPrefix.into(),
+                prefix: LEAF_PREFIX.to_vec(),
+            }),
+            inner_spec: Some(InnerSpec {
+                child_order: vec![0, 1, 2],
+                child_size: 32,
+                min_prefix_length: 0,
+                max_prefix_length: 64,
+                empty_child: vec![0, 32],
+                hash: HashOp::Sha256.into(),
+            }),
+            max_depth: 0,
+            min_depth: 0,
+        }
+    }
 }
