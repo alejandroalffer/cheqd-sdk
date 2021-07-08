9b02688f6472c3a5c7af44170aa611921a420226
Anton Denishchenko <anton.denishchenko@evernym.com>
2021-07-08 04:06:22 +0300

    Add tests for CLI and implementation nested commands
    Signed-off-by: Anton Denishchenko <anton.denishchenko@evernym.com>

--- cli/src/commands/verim_ledger.rs
+++ cli/src/commands/verim_ledger.rs
@@ -8,11 +8,10 @@ use indy::{ErrorCode, IndyError};
 use crate::libindy::payment::Payment;
 use crate::libindy::verim_ledger::VerimLedger;
 use crate::libindy::verim_pool::VerimPool;
+use crate::libindy::verim_keys::VerimKeys;
 
-use serde_json::Value as JSONValue;
+use serde_json::{Value as JSONValue, Value};
 use serde_json::Map as JSONMap;
-
-use crate::utils::table::print_list_table;
 
 pub mod group {
     use super::*;
@@ -20,43 +19,13 @@ pub mod group {
     command_group!(CommandGroupMetadata::new("verim-ledger", "Verim ledger management commands"));
 }
 
-// pub mod parse_query_command {
-//     use super::*;
-//
-//     command!(CommandMetadata::build("parse-query-account-resp", "Create the payment address for specified payment method.")
-//                 .add_required_param("query_resp", "Query response")
-//                 .add_example("payment-address new query_resp=sov")
-//                 .finalize()
-//     );
-//
-//     fn execute(ctx: &CommandContext, params: &CommandParams) -> Result<(), ()> {
-//         trace!("execute >> ctx {:?} params {:?}", ctx, params);
-//         let query_resp = get_str_param("query_resp", params).map_err(error_err!())?;
-//
-//         let res = match VerimLedger::parse_query_account_resp(query_resp) {
-//             Ok(query_resp) => {
-//                 println_succ!("Query response has been taken \"{}\"", query_resp);
-//                 Ok(())
-//             },
-//             Err(err) => {
-//                 handle_indy_error(err, None, None, None);
-//                 Err(())
-//             },
-//         };
-//
-//         trace!("execute << {:?}", res);
-//         res
-//     }
-//
-// }
-
-pub mod build_query_account_command {
+pub mod query_account_command {
     use super::*;
 
-    command!(CommandMetadata::build("build-query-account", "Build query account for verim.")
-                .add_required_param("address", "Address for account")
+    command!(CommandMetadata::build("query-account", "Query account for verim.")
+                .add_required_param("address", "Address of account")
                 .add_required_param("pool_alias", "Alias of pool")
-                .add_example("verim-ledger build-query-account address=sov")
+                .add_example("verim-ledger query-account address=sov pool_alias=my_pool")
                 .finalize()
     );
 
@@ -65,34 +34,129 @@ pub mod build_query_account_command {
         let address = get_str_param("address", params).map_err(error_err!())?;
         let pool_alias = get_str_param("pool_alias", params).map_err(error_err!())?;
 
-        let res = match VerimLedger::build_query_account(address) {
-            Ok(query) =>  {
-                let response = match VerimPool::abci_query(pool_alias, &query) {
-                    Ok(abci_query_response) => {
-                        let parse_query_account_resp = match VerimLedger::parse_query_account_resp(&abci_query_response) {
-                            Ok(_) => {}
-                            Err(err) => {
-                                handle_indy_error(err, None, None, None);
-                                ()
-                            }
-                        };
-                        Ok(parse_query_account_resp)
-                    }
-                    Err(err) => {
-                        handle_indy_error(err, None, None, None);
-                        Err(())
-                    }
-                };
-                Ok(response)
-            },
-            Err(err) => {
-                handle_indy_error(err, None, None, None);
-                Err(())
-            },
-        }.unwrap();
+        let query = VerimLedger::build_query_account(address)
+            .map_err(|err| handle_indy_error(err, None, None, None))?;
+        let response = VerimPool::abci_query(pool_alias, &query)
+            .map_err(|err| handle_indy_error(err, None, None, None))?;
+        let parsed_response = VerimLedger::parse_query_account_resp(&response)
+            .map_err(|err| handle_indy_error(err, None, None, None))?;
 
-        trace!("execute << {:?}", res);
-        res
+        println!("{}",parsed_response);
+        trace!("execute << {:?}", parsed_response);
+
+        Ok(())
     }
+}
 
+pub mod create_nym_command {
+    use super::*;
+    use crate::commands::ledger::{handle_transaction_response, parse_response_with_fees, print_response_receipts, Response};
+
+    command!(CommandMetadata::build("create-nym", "Create nym.")
+                .add_required_param("did", "DID of identity presented in Ledger")
+                .add_required_param("verkey", "Verification key")
+                .add_required_param("alias", "Alias of pool")
+                .add_required_param("address", "Address of wallet")
+                .add_required_param("pubkey", "Public key")
+                .add_required_param("max_coin", "Max coin for transaction")
+                .add_required_param("max_gas", "Max gas for transaction")
+                .add_optional_param("role", "Role of identity. One of: STEWARD, TRUSTEE, TRUST_ANCHOR, ENDORSER, NETWORK_MONITOR or associated number, or empty in case of blacklisting NYM")
+                .add_example("verim-ledger build-query-account address=sov")
+                .finalize()
+    );
+
+    fn execute(ctx: &CommandContext, params: &CommandParams) -> Result<(), ()> {
+        trace!("execute >> ctx {:?} params {:?}", ctx, params);
+        let did = get_str_param("did", params).map_err(error_err!())?;
+        let verkey = get_str_param("verkey", params).map_err(error_err!())?;
+        let alias = get_str_param("alias", params).map_err(error_err!())?;
+        let address = get_str_param("address", params).map_err(error_err!())?;
+        let role = get_str_param("role", params).map_err(error_err!())?;
+        let pubkey = get_str_param("pubkey", params).map_err(error_err!())?;
+        let max_coin = get_str_param("max_coin", params).map_err(error_err!())?;
+        let max_gas = get_str_param("max_gas", params).map_err(error_err!())?;
+
+        let wallet_handle = ensure_opened_wallet_handle(&ctx)?;
+
+        let mut request = VerimLedger::build_msg_create_nym(&did, "creator", verkey, alias, role)
+            .map_err(|err| handle_indy_error(err, None, None, None))?;
+
+        let (account_number, account_sequence) = get_base_account_number_and_sequence(address, alias)?;
+        let tx = VerimLedger::build_tx(
+            alias,
+            pubkey,
+            &request,
+            account_number,
+            account_sequence,
+            max_gas.parse::<u64>().unwrap(),
+            max_coin.parse::<u64>().unwrap(),
+            "token",
+            39039,
+            "memo"
+        ).map_err(|err| handle_indy_error(err, None, None, None))?;
+
+        let signed_tx = VerimKeys::sign(wallet_handle, alias, &tx)
+            .map_err(|err| handle_indy_error(err, None, None, None))?;
+        let response = VerimPool::broadcast_tx_commit(alias, &signed_tx)
+            .map_err(|err| handle_indy_error(err, None, None, None))?;
+        let parsed_response = VerimLedger::parse_msg_create_nym_resp(&response)
+            .map_err(|err| handle_indy_error(err, None, None, None))?;
+
+        println!("{}", parsed_response);
+        trace!("execute << {:?}", parsed_response);
+
+        Ok(())
+    }
+}
+
+fn get_base_account_number_and_sequence(address: &str, pool_alias: &str) -> Result<(u64, u64), ()> {
+    let mut query = VerimLedger::build_query_account(address)
+        .map_err(|err| handle_indy_error(err, None, None, None))?;
+
+    let mut response = VerimPool::abci_query(pool_alias, &query)
+        .map_err(|err| handle_indy_error(err, None, None, None))?;
+
+    let mut parsed_response = VerimLedger::parse_query_account_resp(&response)
+        .map_err(|err| handle_indy_error(err, None, None, None))?;
+
+    let parsed_response: Value = serde_json::from_str(&parsed_response).unwrap();
+    let account = parsed_response["account"].as_object().unwrap();
+    let base_account = account["base_account"].as_object().unwrap();
+    let account_number = base_account["account_number"].as_u64().unwrap();
+    let account_sequence = base_account["sequence"].as_u64().unwrap();
+
+    Ok((account_number, account_sequence))
+}
+
+#[cfg(test)]
+#[cfg(feature = "nullpay_plugin")]
+pub mod tests {
+    use super::*;
+    use crate::commands::common::tests::{load_null_payment_plugin, NULL_PAYMENT_METHOD};
+    use crate::commands::did::tests::SEED_MY1;
+
+    const POOL: &'static str = "pool";
+
+    mod build {
+        use super::*;
+        use crate::commands::ledger::tests::create_address_and_mint_sources;
+        use crate::commands::pool::tests::create_pool;
+
+        #[test]
+        pub fn build_account() {
+            let ctx = setup_with_wallet_and_pool();
+            let payment_address = create_address_and_mint_sources(&ctx);
+            create_pool(&ctx);
+            {
+                let cmd = query_account_command::new();
+                let mut params = CommandParams::new();
+                params.insert("address", payment_address.to_string());
+                params.insert("pool_alias", POOL.to_string());
+                cmd.execute(&ctx, &params).unwrap();
+            }
+            assert!(true);
+
+            tear_down_with_wallet(&ctx);
+        }
+    }
 }
--- cli/src/commands/verim_pool.rs
+++ cli/src/commands/verim_pool.rs
@@ -26,7 +26,7 @@ pub mod add_command {
                 .add_required_param("alias", "Alias for pool.")
                 .add_required_param("rpc_address", "RPC address of pool. Nodes need of RPC pool`s address for connection.")
                 .add_required_param("chain_id", "It marks unique id of network where pool will be created.")
-                .add_example("verim-pool add alias=my_pool address=127.0.0.1 chain_id=verim")
+                .add_example("verim-pool add alias=my_pool address=http://127.0.0.1:26657 chain_id=verim")
                 .finalize()
     );
 
@@ -110,3 +110,64 @@ pub mod abci_info_command {
     }
 }
 
+#[cfg(test)]
+#[cfg(feature = "nullpay_plugin")]
+pub mod tests {
+    use super::*;
+    use crate::commands::common::tests::{load_null_payment_plugin, NULL_PAYMENT_METHOD};
+
+    const POOL: &'static str = "pool";
+    const RPC_ADDRESS: &'static str = "http://127.0.0.1:26657";
+    const CHAIN_ID: &'static str = "verim";
+
+    mod verim_pool {
+        use super::*;
+        use crate::commands::ledger::tests::create_address_and_mint_sources;
+        use crate::commands::pool::tests::create_pool;
+        use crate::commands::verim_ledger::query_account_command;
+
+        #[test]
+        pub fn add_pool() {
+            let ctx = setup_with_wallet_and_pool();
+            {
+                let cmd = add_command::new();
+                let mut params = CommandParams::new();
+                params.insert("rpc_address", RPC_ADDRESS.to_string());
+                params.insert("pool_alias", POOL.to_string());
+                params.insert("chain_id", CHAIN_ID.to_string());
+                cmd.execute(&ctx, &params).unwrap();
+            }
+            assert!(true);
+
+            tear_down_with_wallet(&ctx);
+        }
+
+        #[test]
+        pub fn get_config() {
+            let ctx = setup_with_wallet_and_pool();
+            {
+                let cmd = get_config_command::new();
+                let mut params = CommandParams::new();
+                params.insert("pool_alias", POOL.to_string());
+                cmd.execute(&ctx, &params).unwrap();
+            }
+            assert!(true);
+
+            tear_down_with_wallet(&ctx);
+        }
+
+        #[test]
+        pub fn abci_info() {
+            let ctx = setup_with_wallet_and_pool();
+            {
+                let cmd = abci_info_command::new();
+                let mut params = CommandParams::new();
+                params.insert("pool_alias", POOL.to_string());
+                cmd.execute(&ctx, &params).unwrap();
+            }
+            assert!(true);
+
+            tear_down_with_wallet(&ctx);
+        }
+    }
+}
