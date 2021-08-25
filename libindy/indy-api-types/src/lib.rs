48971c5dc7a436efd68a5d0d2d66dd3e20117b1a
Andrew Nikitin <andrew.nikitin@evernym.com>
2021-08-25 17:14:23 +0300

    Get rid of InvalidState error kind
    Signed-off-by: Andrew Nikitin <andrew.nikitin@evernym.com>

--- libindy/indy-api-types/src/lib.rs
+++ libindy/indy-api-types/src/lib.rs
@@ -268,6 +268,27 @@ pub enum ErrorCode
     // ABCI error from tendermint endpoint
     ABCIError = 800,
 
+    // Cosmos RPC error
+    CosmosRPCError = 801,
+
+    // Error from eyre Report
+    EyreError = 802,
+
+    // Signature error from k256
+    K256SignatureError = 803,
+
+    // Error while using serde module
+    SerdeJSONError = 804,
+
+    // Error from https client
+    HTTPClientError = 805,
+
+    // Protobuf encode error
+    ProstEncodeError = 806,
+
+    // Protobuf decode error
+    ProstDecodeError = 807,
+
 }
 
 pub mod wallet {
