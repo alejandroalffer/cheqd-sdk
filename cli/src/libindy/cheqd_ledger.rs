410cc15695066fdade976b95968f0cf48a366fc2
Andrew Nikitin <andrew.nikitin@evernym.com>
2021-09-07 15:35:53 +0300

    FRemove unused methods
    Signed-off-by: Andrew Nikitin <andrew.nikitin@evernym.com>

--- cli/src/libindy/cheqd_ledger.rs
+++ cli/src/libindy/cheqd_ledger.rs
@@ -58,27 +58,6 @@ impl CheqdLedger {
         cheqd_ledger::bank::parse_query_balance_resp(resp).wait()
     }
 
-    // pub fn build_msg_update_nym(did: &str,
-    //                             creator: &str,
-    //                             verkey: &str,
-    //                             alias: &str,
-    //                             role: &str,
-    //                             id: u64) -> Result<Vec<u8>, IndyError> {
-    //     cheqd_ledger::cheqd::build_msg_update_nym(did, creator, verkey, alias, role, id).wait()
-    // }
-    //
-    // pub fn parse_msg_update_nym_resp(commit_resp: &str) -> Result<String, IndyError> {
-    //     cheqd_ledger::cheqd::parse_msg_update_nym_resp(commit_resp).wait()
-    // }
-    //
-    // pub fn build_msg_delete_nym(creator: &str, id: u64) -> Result<Vec<u8>, IndyError> {
-    //     cheqd_ledger::cheqd::build_msg_delete_nym(creator, id).wait()
-    // }
-    //
-    // pub fn parse_msg_delete_nym_resp(commit_resp: &str) -> Result<String, IndyError> {
-    //     cheqd_ledger::cheqd::parse_msg_delete_nym_resp(commit_resp).wait()
-    // }
-    //
     pub fn build_query_get_nym(id: u64) -> Result<String, IndyError> {
         cheqd_ledger::cheqd::build_query_get_nym(id).wait()
     }
@@ -86,12 +65,4 @@ impl CheqdLedger {
     pub fn parse_query_get_nym_resp(query_resp: &str) -> Result<String, IndyError> {
         cheqd_ledger::cheqd::parse_query_get_nym_resp(query_resp).wait()
     }
-    //
-    // pub fn build_query_all_nym() -> Result<String, IndyError> {
-    //     cheqd_ledger::cheqd::build_query_all_nym().wait()
-    // }
-    //
-    // pub fn parse_query_all_nym_resp(query_resp: &str) -> Result<String, IndyError> {
-    //     cheqd_ledger::cheqd::parse_query_all_nym_resp(query_resp).wait()
-    // }
 }
