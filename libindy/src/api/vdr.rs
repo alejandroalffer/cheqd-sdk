6077ec168219780c58ddcd0171c91b23ceda19f2
Alexey Shklyaev <alexey.shklyaev@evernym.com>
2021-10-06 19:47:25 +0300

    VRD stub API implementation

--- libindy/src/api/vdr.rs
+++ libindy/src/api/vdr.rs
@@ -1,15 +1,48 @@
-use indy_api_types::{
-    CommandHandle, ErrorCode, VdrHandle,
-};
-
+use indy_api_types::{CommandHandle, ErrorCode, errors::prelude::*, INVALID_VDR_HANDLE, VdrHandle};
+use indy_api_types::errors::IndyResult;
+use indy_utils::ctypes;
 use libc::c_char;
+
+use crate::Locator;
+use crate::services::CommandMetric;
 
 #[no_mangle]
 pub extern "C" fn indy_vdr_create(
     command_handle: CommandHandle,
     cb: Option<extern "C" fn(command_handle_: CommandHandle, err: ErrorCode, handle: VdrHandle)>,
 ) -> ErrorCode {
-    unimplemented!()
+    debug!("indy_vdr_create >");
+
+    check_useful_c_callback!(cb, ErrorCode::CommonInvalidParam2);
+
+    debug!("indy_vdr_create ?");
+
+    let locator = Locator::instance();
+
+    // let action = async move {
+    //     let res = locator
+    //         .vdr_controller
+    //         .create_vdr()
+    //         .await;
+    //     res
+    // };
+
+    let cb = move |res: IndyResult<_>| {
+        let (err, vdr_handle) = prepare_result!(res, INVALID_VDR_HANDLE);
+
+        debug!(
+            "indy_vdr_create ? err {:?} vdr_handle {:?}",
+            err, vdr_handle
+        );
+
+        cb(command_handle, err, vdr_handle)
+    };
+
+    // locator.executor.spawn_ok_instrumented(CommandMetric::VdrCommandCreateVdr, action, cb);
+
+    let res = ErrorCode::Success;
+    debug!("indy_vdr_create > {:?}", res);
+    res
 }
 
 #[no_mangle]
@@ -21,7 +54,44 @@ pub extern "C" fn indy_vdr_register_indy_ledger(
     taa_config: *const c_char,
     cb: Option<extern "C" fn(command_handle_: CommandHandle, err: ErrorCode)>,
 ) -> ErrorCode {
-    unimplemented!()
+    debug!(
+        "indy_vdr_register_indy_ledger > handle {:?} namespace_list {:?} genesis_txn_data {:?} taa_config {:?}",
+        handle, namespace_list, genesis_txn_data, taa_config
+    );
+
+    check_useful_c_str!(namespace_list, ErrorCode::CommonInvalidParam3);
+    check_useful_c_str!(genesis_txn_data, ErrorCode::CommonInvalidParam4);
+    check_useful_c_str!(taa_config, ErrorCode::CommonInvalidParam5);
+    check_useful_c_callback!(cb, ErrorCode::CommonInvalidParam6);
+
+    debug!(
+        "indy_vdr_register_indy_ledger ? handle {:?} namespace_list {:?} genesis_txn_data {:?} taa_config {:?}",
+        handle, namespace_list, genesis_txn_data, taa_config
+    );
+
+    let locator = Locator::instance();
+
+    // let action = async move {
+    //     let res = locator
+    //         .vdr_controller
+    //         .register_indy_ledger(&namespace_list, &genesis_txn_data, &taa_config)
+    //         .await;
+    //     res
+    // };
+
+    let cb = move |res: IndyResult<_>| {
+        let err = prepare_result!(res);
+
+        debug!("indy_vdr_register_indy_ledger ? err {:?} ", err);
+
+        cb(command_handle, err)
+    };
+
+    // locator.executor.spawn_ok_instrumented(CommandMetric::VdrCommandRegisterIndyLedger, action, cb);
+
+    let res = ErrorCode::Success;
+    debug!("indy_vdr_register_indy_ledger > {:?}", res);
+    res
 }
 
 #[no_mangle]
@@ -33,7 +103,44 @@ pub extern "C" fn indy_vdr_register_cheqd_ledger(
     node_addrs_list: *const c_char,
     cb: Option<extern "C" fn(command_handle_: CommandHandle, err: ErrorCode)>,
 ) -> ErrorCode {
-    unimplemented!();
+    debug!(
+        "indy_vdr_register_cheqd_ledger > handle {:?} namespace_list {:?} chain_id {:?} node_addrs_list {:?}",
+        handle, namespace_list, chain_id, node_addrs_list
+    );
+
+    check_useful_c_str!(namespace_list, ErrorCode::CommonInvalidParam3);
+    check_useful_c_str!(chain_id, ErrorCode::CommonInvalidParam4);
+    check_useful_c_str!(node_addrs_list, ErrorCode::CommonInvalidParam5);
+    check_useful_c_callback!(cb, ErrorCode::CommonInvalidParam6);
+
+    debug!(
+        "indy_vdr_register_cheqd_ledger ? handle {:?} namespace_list {:?} chain_id {:?} node_addrs_list {:?}",
+        handle, namespace_list, chain_id, node_addrs_list
+    );
+
+    let locator = Locator::instance();
+
+    // let action = async move {
+    //     let res = locator
+    //         .vdr_controller
+    //         .register_cheqd_ledger(&namespace_list, &chain_id, &node_addrs_list)
+    //         .await;
+    //     res
+    // };
+
+    let cb = move |res: IndyResult<_>| {
+        let err = prepare_result!(res);
+
+        debug!("indy_vdr_register_cheqd_ledger ? err {:?} ", err);
+
+        cb(command_handle, err)
+    };
+
+    // locator.executor.spawn_ok_instrumented(CommandMetric::VdrCommandRegisterCheqdLedger, action, cb);
+
+    let res = ErrorCode::Success;
+    debug!("indy_vdr_register_cheqd_ledger > {:?}", res);
+    res
 }
 
 #[no_mangle]
@@ -43,7 +150,36 @@ pub extern "C" fn indy_vdr_ping(
     namespace_list: *const c_char,
     cb: Option<extern "C" fn(command_handle_: CommandHandle, err: ErrorCode, status_list: *const c_char)>,
 ) -> ErrorCode {
-    unimplemented!();
+    debug!("indy_vdr_ping > handle {:?} namespace_list {:?}", handle, namespace_list);
+
+    check_useful_c_str!(namespace_list, ErrorCode::CommonInvalidParam3);
+    check_useful_c_callback!(cb, ErrorCode::CommonInvalidParam4);
+
+    debug!("indy_vdr_ping ? handle {:?} namespace_list {:?} ", handle, namespace_list);
+
+    let locator = Locator::instance();
+
+    // let action = async move {
+    //     let res = locator
+    //         .vdr_controller
+    //         .ping(&namespace_list)
+    //         .await;
+    //     res
+    // };
+
+    let cb = move |res: IndyResult<_>| {
+        let (err, status_list) = prepare_result!(res, String::new());
+
+        debug!("indy_vdr_ping ? err {:?} status_list {:?}", err, status_list);
+
+        cb(command_handle, err, status_list)
+    };
+
+    // locator.executor.spawn_ok_instrumented(CommandMetric::VdrCommandPing, action, cb);
+
+    let res = ErrorCode::Success;
+    debug!("indy_vdr_ping > {:?}", res);
+    res
 }
 
 #[no_mangle]
@@ -52,7 +188,35 @@ pub extern "C" fn indy_vdr_cleanup(
     handle: VdrHandle,
     cb: Option<extern "C" fn(command_handle_: CommandHandle, err: ErrorCode)>,
 ) -> ErrorCode {
-    unimplemented!();
+    debug!("indy_vdr_cleanup >");
+
+    check_useful_c_callback!(cb, ErrorCode::CommonInvalidParam3);
+
+    debug!("indy_vdr_cleanup ?");
+
+    let locator = Locator::instance();
+
+    // let action = async move {
+    //     let res = locator
+    //         .vdr_controller
+    //         .cleanup()
+    //         .await;
+    //     res
+    // };
+
+    let cb = move |res: IndyResult<_>| {
+        let err = prepare_result!(res);
+
+        debug!("indy_vdr_cleanup ? err {:?} ", err);
+
+        cb(command_handle, err)
+    };
+
+    // locator.executor.spawn_ok_instrumented(CommandMetric::VdrCommandCleanup, action, cb);
+
+    let res = ErrorCode::Success;
+    debug!("indy_vdr_cleanup > {:?}", res);
+    res
 }
 
 #[no_mangle]
@@ -63,18 +227,90 @@ pub extern "C" fn indy_vdr_resolve_did(
     cache_options: *const c_char,
     cb: Option<extern "C" fn(command_handle_: CommandHandle, err: ErrorCode, diddoc: *const c_char)>,
 ) -> ErrorCode {
-    unimplemented!();
+    debug!(
+        "indy_vdr_resolve_did > handle {:?} fqdid {:?} cache_options {:?}",
+        handle, fqdid, cache_options
+    );
+
+    check_useful_c_str!(fqdid, ErrorCode::CommonInvalidParam3);
+    check_useful_c_str!(cache_options, ErrorCode::CommonInvalidParam4);
+    check_useful_c_callback!(cb, ErrorCode::CommonInvalidParam5);
+
+    debug!(
+        "indy_vdr_resolve_did ? handle {:?} fqdid {:?} cache_options {:?}",
+        handle, fqdid, cache_options
+    );
+
+    let locator = Locator::instance();
+
+    // let action = async move {
+    //     let res = locator
+    //         .vdr_controller
+    //         .reslove_did(&fqdid, &cache_options)
+    //         .await;
+    //     res
+    // };
+
+    let cb = move |res: IndyResult<_>| {
+        let (err, diddoc) = prepare_result!(res, String::new());
+
+        debug!("indy_vdr_resolve_did ? err {:?} diddoc {:?}", err, diddoc);
+
+        cb(command_handle, err, diddoc)
+    };
+
+    // locator.executor.spawn_ok_instrumented(CommandMetric::VdrCommandResolveDid, action, cb);
+
+    let res = ErrorCode::Success;
+    debug!("indy_vdr_resolve_did > {:?}", res);
+    res
 }
 
 #[no_mangle]
 pub extern "C" fn indy_vdr_resolve_schema(
     command_handle: CommandHandle,
     handle: VdrHandle,
-    fqdid: *const c_char,
+    fqschema: *const c_char,
     cache_options: *const c_char,
     cb: Option<extern "C" fn(command_handle_: CommandHandle, err: ErrorCode, schema: *const c_char)>,
 ) -> ErrorCode {
-    unimplemented!();
+    debug!(
+        "indy_vdr_resolve_schema > handle {:?} fqschema {:?} cache_options {:?}",
+        handle, fqschema, cache_options
+    );
+
+    check_useful_c_str!(fqschema, ErrorCode::CommonInvalidParam3);
+    check_useful_c_str!(cache_options, ErrorCode::CommonInvalidParam4);
+    check_useful_c_callback!(cb, ErrorCode::CommonInvalidParam5);
+
+    debug!(
+        "indy_vdr_resolve_schema ? handle {:?} fqschema {:?} cache_options {:?}",
+        handle, fqschema, cache_options
+    );
+
+    let locator = Locator::instance();
+
+    // let action = async move {
+    //     let res = locator
+    //         .vdr_controller
+    //         .resolve_schema(&fqschema, &cache_options)
+    //         .await;
+    //     res
+    // };
+
+    let cb = move |res: IndyResult<_>| {
+        let (err, schema) = prepare_result!(res, String::new());
+
+        debug!("indy_vdr_resolve_schema ? err {:?} schema {:?}", err, schema);
+
+        cb(command_handle, err, schema)
+    };
+
+    // locator.executor.spawn_ok_instrumented(CommandMetric::VdrCommandResolveSchema, action, cb);
+
+    let res = ErrorCode::Success;
+    debug!("indy_vdr_resolve_schema > {:?}", res);
+    res
 }
 
 #[no_mangle]
@@ -85,7 +321,43 @@ pub extern "C" fn indy_vdr_resolve_cred_def(
     cache_options: *const c_char,
     cb: Option<extern "C" fn(command_handle_: CommandHandle, err: ErrorCode, cred_def: *const c_char)>,
 ) -> ErrorCode {
-    unimplemented!();
+    debug!(
+        "indy_vdr_resolve_cred_def > handle {:?} fqcreddef {:?} cache_options {:?}",
+        handle, fqcreddef, cache_options
+    );
+
+    check_useful_c_str!(fqcreddef, ErrorCode::CommonInvalidParam3);
+    check_useful_c_str!(cache_options, ErrorCode::CommonInvalidParam4);
+    check_useful_c_callback!(cb, ErrorCode::CommonInvalidParam5);
+
+    debug!(
+        "indy_vdr_resolve_cred_def ? handle {:?} fqcreddef {:?} cache_options {:?}",
+        handle, fqcreddef, cache_options
+    );
+
+    let locator = Locator::instance();
+
+    // let action = async move {
+    //     let res = locator
+    //         .vdr_controller
+    //         .resolve_cred_def(&fqschema, &cache_options)
+    //         .await;
+    //     res
+    // };
+
+    let cb = move |res: IndyResult<_>| {
+        let (err, cred_def) = prepare_result!(res, String::new());
+
+        debug!("indy_vdr_resolve_cred_def ? err {:?} cred_def {:?}", err, cred_def);
+
+        cb(command_handle, err, cred_def)
+    };
+
+    // locator.executor.spawn_ok_instrumented(CommandMetric::VdrCommandResolveCredDef, action, cb);
+
+    let res = ErrorCode::Success;
+    debug!("indy_vdr_resolve_cred_def > {:?}", res);
+    res
 }
 
 #[no_mangle]
@@ -97,7 +369,44 @@ pub extern "C" fn indy_vdr_prepare_did(
     endorser: *const c_char,
     cb: Option<extern "C" fn(command_handle_: CommandHandle, err: ErrorCode, prepared_txn: *const c_char)>,
 ) -> ErrorCode {
-    unimplemented!();
+    debug!(
+        "indy_vdr_prepare_did > handle {:?} txn_specific_params {:?} submitter_did {:?} endorser {:?}",
+        handle, txn_specific_params, submitter_did, endorser
+    );
+
+    check_useful_c_str!(txn_specific_params, ErrorCode::CommonInvalidParam3);
+    check_useful_c_str!(submitter_did, ErrorCode::CommonInvalidParam4);
+    check_useful_opt_c_str!(endorser, ErrorCode::CommonInvalidParam5);
+    check_useful_c_callback!(cb, ErrorCode::CommonInvalidParam6);
+
+    debug!(
+        "indy_vdr_prepare_did ? handle {:?} txn_specific_params {:?} submitter_did {:?} endorser {:?}",
+        handle, txn_specific_params, submitter_did, endorser
+    );
+
+    let locator = Locator::instance();
+
+    // let action = async move {
+    //     let res = locator
+    //         .vdr_controller
+    //         .prepare_did(&txn_specific_params, &submitter_did, &endorser)
+    //         .await;
+    //     res
+    // };
+
+    let cb = move |res: IndyResult<_>| {
+        let (err, prepared_txn) = prepare_result!(res, String::new());
+
+        debug!("indy_vdr_prepare_did ? err {:?} prepared_txn {:?}", err, prepared_txn);
+
+        cb(command_handle, err, prepared_txn)
+    };
+
+    // locator.executor.spawn_ok_instrumented(CommandMetric::VdrCommandPrepareDid, action, cb);
+
+    let res = ErrorCode::Success;
+    debug!("indy_vdr_prepare_did > {:?}", res);
+    res
 }
 
 #[no_mangle]
@@ -109,7 +418,44 @@ pub extern "C" fn indy_vdr_prepare_schema(
     endorser: *const c_char,
     cb: Option<extern "C" fn(command_handle_: CommandHandle, err: ErrorCode, prepared_txn: *const c_char)>,
 ) -> ErrorCode {
-    unimplemented!();
+    debug!(
+        "indy_vdr_prepare_schema > handle {:?} txn_specific_params {:?} submitter_did {:?} endorser {:?}",
+        handle, txn_specific_params, submitter_did, endorser
+    );
+
+    check_useful_c_str!(txn_specific_params, ErrorCode::CommonInvalidParam3);
+    check_useful_c_str!(submitter_did, ErrorCode::CommonInvalidParam4);
+    check_useful_opt_c_str!(endorser, ErrorCode::CommonInvalidParam5);
+    check_useful_c_callback!(cb, ErrorCode::CommonInvalidParam6);
+
+    debug!(
+        "indy_vdr_prepare_schema ? handle {:?} txn_specific_params {:?} submitter_did {:?} endorser {:?}",
+        handle, txn_specific_params, submitter_did, endorser
+    );
+
+    let locator = Locator::instance();
+
+    // let action = async move {
+    //     let res = locator
+    //         .vdr_controller
+    //         .prepare_schema(&txn_specific_params, &submitter_did, &endorser)
+    //         .await;
+    //     res
+    // };
+
+    let cb = move |res: IndyResult<_>| {
+        let (err, prepared_txn) = prepare_result!(res, String::new());
+
+        debug!("indy_vdr_prepare_schema ? err {:?} prepared_txn {:?}", err, prepared_txn);
+
+        cb(command_handle, err, prepared_txn)
+    };
+
+    // locator.executor.spawn_ok_instrumented(CommandMetric::VdrCommandPrepareSchema, action, cb);
+
+    let res = ErrorCode::Success;
+    debug!("indy_vdr_prepare_schema > {:?}", res);
+    res
 }
 
 #[no_mangle]
@@ -121,7 +467,44 @@ pub extern "C" fn indy_vdr_prepare_cred_def(
     endorser: *const c_char,
     cb: Option<extern "C" fn(command_handle_: CommandHandle, err: ErrorCode, prepared_txn: *const c_char)>,
 ) -> ErrorCode {
-    unimplemented!();
+    debug!(
+        "indy_vdr_prepare_cred_def > handle {:?} txn_specific_params {:?} submitter_did {:?} endorser {:?}",
+        handle, txn_specific_params, submitter_did, endorser
+    );
+
+    check_useful_c_str!(txn_specific_params, ErrorCode::CommonInvalidParam3);
+    check_useful_c_str!(submitter_did, ErrorCode::CommonInvalidParam4);
+    check_useful_opt_c_str!(endorser, ErrorCode::CommonInvalidParam5);
+    check_useful_c_callback!(cb, ErrorCode::CommonInvalidParam6);
+
+    debug!(
+        "indy_vdr_prepare_cred_def ? handle {:?} txn_specific_params {:?} submitter_did {:?} endorser {:?}",
+        handle, txn_specific_params, submitter_did, endorser
+    );
+
+    let locator = Locator::instance();
+
+    // let action = async move {
+    //     let res = locator
+    //         .vdr_controller
+    //         .prepare_cred_def(&txn_specific_params, &submitter_did, &endorser)
+    //         .await;
+    //     res
+    // };
+
+    let cb = move |res: IndyResult<_>| {
+        let (err, prepared_txn) = prepare_result!(res, String::new());
+
+        debug!("indy_vdr_prepare_cred_def ? err {:?} prepared_txn {:?}", err, prepared_txn);
+
+        cb(command_handle, err, prepared_txn)
+    };
+
+    //locator.executor.spawn_ok_instrumented(CommandMetric::VdrCommandPrepareCredDef, action, cb);
+
+    let res = ErrorCode::Success;
+    debug!("indy_vdr_prepare_cred_def > {:?}", res);
+    res
 }
 
 #[no_mangle]
@@ -135,5 +518,52 @@ pub extern "C" fn indy_vdr_submit_txn(
     endorsement_len: u32,
     cb: Option<extern "C" fn(command_handle_: CommandHandle, err: ErrorCode, status: *const c_char)>,
 ) -> ErrorCode {
-    unimplemented!();
+    debug!(
+        "indy_vdr_submit_txn > handle {:?} prepared_txn {:?} signature_raw {:?} signature_len {:?} endorsement_raw {:?} endorsement_len {:?}",
+        handle, prepared_txn, signature_raw, signature_len, endorsement_raw, endorsement_len
+    );
+
+    check_useful_c_str!(prepared_txn, ErrorCode::CommonInvalidParam3);
+    check_useful_c_byte_array!(
+        signature_raw,
+        signature_len,
+        ErrorCode::CommonInvalidParam4,
+        ErrorCode::CommonInvalidParam5
+    );
+    check_useful_c_byte_array!(
+        endorsement_raw,
+        endorsement_len,
+        ErrorCode::CommonInvalidParam6,
+        ErrorCode::CommonInvalidParam7
+    );
+    check_useful_c_callback!(cb, ErrorCode::CommonInvalidParam8);
+
+    debug!(
+        "indy_vdr_submit_txn ? handle {:?} prepared_txn {:?} signature_raw {:?} signature_len {:?} endorsement_raw {:?} endorsement_len {:?}",
+        handle, prepared_txn, signature_raw, signature_len, endorsement_raw, endorsement_len
+    );
+
+    let locator = Locator::instance();
+
+    // let action = async move {
+    //     let res = locator
+    //         .vdr_controller
+    //         .submit_txn(&prepared_txn, &signature_raw, &endorsement_raw)
+    //         .await;
+    //     res
+    // };
+
+    let cb = move |res: IndyResult<_>| {
+        let (err, status) = prepare_result!(res, String::new());
+
+        debug!("indy_vdr_submit_txn ? err {:?} status {:?}", err, status);
+
+        cb(command_handle, err, status)
+    };
+
+    //locator.executor.spawn_ok_instrumented(CommandMetric::VdrCommandSubmitTxn, action, cb);
+
+    let res = ErrorCode::Success;
+    debug!("indy_vdr_submit_txn > {:?}", res);
+    res
 }
